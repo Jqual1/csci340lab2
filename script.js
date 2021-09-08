f294ac141ef01066488744e596cc756b77e02d12
Jonathon <jonathonq02@gmail.com>
2021-09-07 22:22:54 -0500

    Reorganized

--- script.js
+++ script.js
@@ -25,4 +25,8 @@ $(document).ready(function() {
       }
     })
   });
+  $('#click').click(function(){
+    $('#fox').attr("src", "images/blank.png");
+    $('#quote').text('01001000 01100101 01101100 01101100 01101111 00100001');
+  })
 });
